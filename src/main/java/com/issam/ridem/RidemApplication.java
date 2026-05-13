f8de172c437241c474df28e396a15bcd1abae03d
Issam Channouf <117946948+IssamChannouf@users.noreply.github.com>
2026-05-13 19:59:31 +0200

    chore: initialize Spring Boot project with base configuration

---
+++ src/main/java/com/issam/ridem/RidemApplication.java
@@ -0,0 +1,13 @@
+package com.issam.ridem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issam/ridem/RidemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issam.ridem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
